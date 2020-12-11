86bc1e8ee11ac54eb818f19fe54546184d57ebb6
Limunini <58999148+Limunini@users.noreply.github.com>
2020-12-11 14:39:38 +0200

    lecture 10
    10

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,9 @@
+console.log("ASSIGNMENT")
+
+let country = "South Africa"
+let continent = "Africa"
+let population = 80
+
+console.log(country)
+console.log(continent)
+console.log(population)
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,24 @@
+console.log("LECTURE")
 let js = 'amazing';
-if (js === 'amazing') alert('JS is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Bob";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+
+
