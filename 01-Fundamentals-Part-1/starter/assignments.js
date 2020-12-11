f0a7e2e7ce4335ff4ab5c00e177097ce61ae8f1d
Limunini <58999148+Limunini@users.noreply.github.com>
2020-12-11 15:08:41 +0200

    lecture 12
    lecture 12

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,6 @@
-console.log("ASSIGNMENT")
-
+/*
+console.log("ASSIGNMENT 10");
+//VARIABLES
 let country = "South Africa"
 let continent = "Africa"
 let population = 80
@@ -7,3 +8,21 @@ let population = 80
 console.log(country)
 console.log(continent)
 console.log(population)
+
+console.log("ASSIGNMENT 12");
+//DATA TYPES
+let isIsland = false
+let language;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+
+language = "Afrikaans"
+
+console.log(language);
+console.log(typeof language);
+
+*/
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,6 @@
-console.log("LECTURE")
+/*
+console.log("LECTURE 10");
+//VARIABLES
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -7,8 +9,10 @@ console.log(23);
 
 let firstName = "Bob";
 console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+//console.log(firstName);
+//console.log(firstName);
+
+// Variable name Conventions
 
 let person = "Jonas";
 let PI = 3.1415;
@@ -20,5 +24,37 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+*/
+/*
+console.log("LECTURE 12");
+//DATA TYPES
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+console.log(typeof "Jonas");
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2020;
+console.log(year);
+console.log(typeof year)
+
+//JS bug
+console.log(typeof null)
+*/
+
+
+
+
+
 
 
