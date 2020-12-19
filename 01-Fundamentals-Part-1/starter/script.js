c5f7cc09ac6f62021c7a2a7963d2c2248fe3adc7
Limunini <58999148+Limunini@users.noreply.github.com>
2020-12-19 20:07:37 +0200

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -217,4 +217,34 @@ n = '10' - '4' - '3' - 2 +'5';  //15
 console.log("LECTURE 21");
 //Truthy and Falsy Values
 
+//5 Falsy Values - not yet false
+//  0, ' ', undefined, null, NaN
+
+//Falsy
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(''));
+//Truthy
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all!");
+} else {
+    console.log('You should get a job!');
+}
+
+let height;
+if(height) {
+    console.log("Yay! Height is defined!");
+} else {
+    console.log(`Height is UNDEFINED`);
+}
+
+
+
+
+
+
 
