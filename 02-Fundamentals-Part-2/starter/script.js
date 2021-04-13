d551e870cac069cb5d6598249ea88ff63d5fbb93
Limunini <58999148+Limunini@users.noreply.github.com>
2021-04-13 11:58:56 +0200

    added assignment + lecture 33

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -20,3 +20,24 @@ if (hasDriverLicense) console.log('I can drive :D')
 
 console.log('CLASS 33')
   //  Functions
+
+function logger() {
+  console.log('My name is Jonas');
+}
+
+//  calling / running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+//  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice)
